6211f86bdb7d1a27d6e2540961958c94f003294b
ashish.ghumnar <ashish.ghumnar@rakuten.com>
2021-07-19 23:28:11 +0900

    adding precommit

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -5,6 +5,6 @@ import App from "./../App";
 describe("Test App Entry point", function () {
   it("should have a header tag with Hello CodeSandbox", function () {
     const wrapper = shallow(<App />);
-    expect(wrapper.find("h1").text()).toEqual("Hello Codeandbox");
+    expect(wrapper.find("h1").text()).toEqual("Hello CodeSandbox");
   });
 });
