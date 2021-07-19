03846382b143102923032fe542f27098503faab2
ashish.ghumnar <ashish.ghumnar@rakuten.com>
2021-07-19 15:49:02 +0900

    init project

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import App from "./../App";
+
+describe("Test App Entry point", function () {
+  it("should have a header tag with Hello CodeSandbox", function () {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toEqual("Hello CodeSandbox");
+  });
+});
